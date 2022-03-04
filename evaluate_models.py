a273c00c298e70f33987b557808acae0bd18a59c
root <root@Dream-Gaming.dreamwaker.net>
2022-03-03 20:09:02 -0700

    Initial Commit

---
+++ evaluate_models.py
@@ -0,0 +1,53 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from xgboost import XGBClassifier
+from sklearn import model_selection
+from sklearn.utils import class_weight
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+import numpy as np
+import pandas as pd
+
+
+def run_exps(X_train: pd.DataFrame , y_train: pd.DataFrame, X_test: pd.DataFrame, y_test: pd.DataFrame) -> pd.DataFrame:
+    '''
+    Lightweight script to test many models and find winners
+    :param X_train: training split
+    :param y_train: training target vector
+    :param X_test: test split
+    :param y_test: test target vector
+    :return: DataFrame of predictions
+    '''
+    
+    dfs = []
+    models = [
+          ('LogReg', LogisticRegression()), 
+          ('RF', RandomForestClassifier()),
+          ('KNN', KNeighborsClassifier()),
+          ('SVM', SVC()), 
+          ('GNB', GaussianNB()),
+          ('XGB', XGBClassifier())
+        ]
+    results = []
+    names = []
+    scoring = ['accuracy', 'precision_weighted', 'recall_weighted', 'f1_weighted', 'roc_auc']
+    target_names = ['sporty', 'formal']
+    
+    for name, model in models:
+        kfold = model_selection.KFold(n_splits=5, shuffle=True, random_state=90210)
+        cv_results = model_selection.cross_validate(model, X_train, y_train, cv=kfold, scoring=scoring)
+        clf = model.fit(X_train, y_train)
+        y_pred = clf.predict(X_test)
+        print(name)
+        print(classification_report(y_test, y_pred, target_names=target_names))
+        results.append(cv_results)
+        names.append(name)
+        this_df = pd.DataFrame(cv_results)
+        this_df['model'] = name
+        dfs.append(this_df)
+        final = pd.concat(dfs, ignore_index=True)
+        
+    return final
---
+++ preprocessing.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
+from scipy import stats
+from matplotlib.pyplot import figure
+from sklearn.impute import SimpleImputer
+
+import seaborn as sns
+import statsmodels.api as sm
+
+analysis_name = "Clothing Type Classifier"
+
+#Import into pandas
+dataset = pd.read_csv('dataset_raw.csv')
+
+#Drop irrelevant columns
+dataset = dataset.drop("scrape_date", axis=1)
+dataset = dataset.drop("upper_material", axis=1)
+dataset = dataset.drop("domain", axis=1)
+dataset = dataset.drop("url", axis=1)
+dataset = dataset.drop("referer_url", axis=1)
+dataset = dataset.drop("brand_url", axis=1)
+
+#Remove all columns that are all NaN
+dataset = dataset.dropna(how='all', axis=1)
+
+#Impute our missing categoricak data (there are lots!)
+#imputer = SimpleImputer(strategy="most_frequent")
+#dataset = imputer.fit_transform(dataset)
+
+print(dataset.head())
+
+dataset.to_csv('dataset_processed.csv')
